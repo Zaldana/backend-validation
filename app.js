17fa51d4197c488f32bbdb1619e29fec6bfb3ece
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-11-05 09:06:52 -0400

    updates for react frontend

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose");
+var cors = require("cors")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users/usersRouter');
@@ -21,6 +22,7 @@ mongoose.connect(process.env.MONGO_DB)
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
